4bb6a2bddcbf07e3cdb4386fe51a132a415bd3cf
Sheep <64995222+TeivaScouarnec@users.noreply.github.com>
2023-01-05 18:07:00 +0100

    +inTable(); +addWord();  +removeWord(); +modifyWord()

--- model/database.py
+++ model/database.py
@@ -5,7 +5,7 @@ import time
 class database:
     connexion : dict = {}
 
-    def __init__(self,host:str,user:str,password:str,database:str) -> None:
+    def __init__(self,host:str,user:str,password:str,database:str) -> None: #Initialise la base de donnée
         self.connexion = {
             "host" : host,
             "user" : user,
@@ -14,57 +14,244 @@ class database:
         }
         self.createDatabase()
 
-    def createDatabase(self):
-        print("[DATABASE] "+time.strftime("%x, %X,")+" Création de la base de donnée et de ses tables.")
+    def createDatabase(self): #Création de la base de donnée et de ses tables
+        currentTime = time.strftime("%x-%X")
+        print(f"[DATABASE][CREATEDATABASE] {currentTime} Création de la base de donnée et de ses tables.")
+        
         try:
             with mysql.connector.connect(**self.connexion) as db:
                 db.autocommit = True
                 with db.cursor() as c:
-                    try:
+                    
+                    try: #Créer la table french
                         c.execute("""
                             CREATE TABLE IF NOT EXISTS `french`(
                                 `ID` int(255) NOT NULL AUTO_INCREMENT,
-                                `normandID` int(255),
-                                `expression` varchar(255) NOT NULL,
+                                `normandID` int(255) DEFAULT NULL,
+                                `word` varchar(255) NOT NULL,
                                 PRIMARY KEY (`ID`)
                             );
                         """)
-                        print("[DATABASE] "+time.strftime("%x, %X,")+" La table 'french' a été crée.")
-                    except:
-                        print("[DATABASE] "+time.strftime("%x, %X,")+" La table 'french' n'a pas pu être crée!")
+                        print(f"[DATABASE][CREATEDATABASE] {currentTime} La table 'french' a été crée.")
+                    except mysql.connector.Error as err:
+                        error = str(err)
+                        print(f"[DATABSE][CREATEDATABASE] {currentTime} {error}")
                     
-                    try:
+                    try: #Créer la table normand
                         c.execute("""
                             CREATE TABLE IF NOT EXISTS `normand`(
                                 `ID` int(255) NOT NULL AUTO_INCREMENT,
-                                `frenchID` int(255),
-                                `expression` varchar(255) NOT NULL,
+                                `frenchID` int(255) DEFAULT NULL,
+                                `word` varchar(255) NOT NULL,
                                 PRIMARY KEY (`ID`)
                             );
                         """)
-                        print("[DATABASE] "+time.strftime("%x, %X,")+" La table 'normand' a été crée.")
-                    except:
-                        print("[DATABASE] "+time.strftime("%x, %X,")+" La table 'normand' n'a pas pu être crée!")
+                        print(f"[DATABASE][CREATEDATABASE] {currentTime} La table 'normand' a été crée.")
+                    except mysql.connector.Error as err:
+                        error = str(err)
+                        print(f"[DATABSE][CREATEDATABASE] {currentTime} {error}")
 
-                    try:
+                    try: #Ajoutes à la colonne normandID une clé étrangère de la table french
                         c.execute("""
-                            ALTER TABLE french ADD FOREIGN KEY (normandID) REFERENCES normand(ID);
+                            ALTER TABLE french
+                            ADD CONSTRAINT fk_french_normand_id
+                            FOREIGN KEY (normandID) REFERENCES normand(ID)
                         """)
-                        print("[DATABASE] "+time.strftime("%x, %X,")+" L'ID de la table 'normand' a été rajouté dans la colonne 'normandID' de la table 'french'")
-                    except:
-                        print("[DATABASE] "+time.strftime("%x, %X,")+" L'ID de la table 'normand' n'a pas pu être rajouté dans la colonne 'normandID' de la table 'french'")
+                        print(f"[DATABASE][CREATEDATABASE] {currentTime} La clé étrangère 'fk_french_normand_id' a été liée à la colonne normandID de la table french.")
+                    except mysql.connector.Error as err:
+                        error = str(err)
+                        print(f"[DATABSE][CREATEDATABASE] {currentTime} {error}")
 
-                    try:
+                    try: #Ajoutes à la colonne frenchID une clé étrangère de la table normand
                         c.execute("""
-                            ALTER TABLE normand ADD FOREIGN KEY (frenchID) REFERENCES french(ID);
+                            ALTER TABLE normand
+                            ADD CONSTRAINT fk_normand_french_id
+                            FOREIGN KEY (frenchID) REFERENCES french(ID)
                         """)
-                        print("[DATABASE] "+time.strftime("%x, %X,")+" L'ID de la table 'french' a été rajouté dans la colonne 'frenchID' de la table 'normand'")
-                    except:
-                        print("[DATABASE] "+time.strftime("%x, %X,")+" L'ID de la table 'french' n'a pas pu être rajouté dans la colonne 'frenchID' de la table 'normand'")
-            print("[DATABASE] "+time.strftime("%x, %X,")+" Création des tables terminées.")
-        except:
-            print("[DATABASE] "+time.strftime("%x, %X,")+" Impossible de se connecter à les tables.")
+                        print(f"[DATABASE][CREATEDATABASE] {currentTime} La clé étrangère 'fk_normand_french_id' a été liée à la colonne frenchID de la table normand.")
+                    except mysql.connector.Error as err:
+                        error = str(err)
+                        print(f"[DATABSE][CREATEDATABASE] {currentTime} {error}")
 
+            print(f"[DATABASE][CREATEDATABASE] {currentTime} Création des tables terminées.")
+            db.close()
+
+        except mysql.connector.Error as err:
+            error1 = str(err)
+            print(f"[DATABASE][INTABLE] {currentTime} Impossible de se connecter à la base de données")
+            print(f"[DATABASE][INTABLE] {currentTime} {error1}")
+            db.close()
+
+    def inTable(self,wordFrench:str,wordNormand:str): #Vérifies si les mots existent dans les tables
+        currentTime = time.strftime("%x-%X")
+        print(f"[DATABASE][INTABLE] {currentTime} Vérification du mot {wordFrench} dans la table 'french'.")
+
+        try:
+            with mysql.connector.connect(**self.connexion) as db:
+                with db.cursor() as c:
+                    
+                    try: #Récupères la donnée dans la colonne word de la table french
+                        c.execute(f"""
+                            SELECT word
+                            FROM french
+                            WHERE EXISTS (
+                                SELECT word
+                                FROM french
+                                WHERE word = '{wordFrench}'
+                            )
+                        """)
+                        result = c.fetchmany(1)
+
+                        if (len(result) > 0): 
+                            print(f"[DATABASE][INTABLE] {currentTime} le mot {wordFrench} est présent dans la table 'french'.")
+                            return True
+                        else:
+                            print(f"[DATABASE][INTABLE] {currentTime} le mot {wordFrench} n'est pas présent dans la table 'french'.")
+                    except mysql.connector.Error as err:
+                        error1 = str(err)
+                        db.rollback()
+                        db.close()
+                        print(f"[DATABSE][INTABLE] {currentTime} {error1}")
+                        return True
+
+                    try: #Récupères la donnée dans la colonne word de la table normand
+                        c.execute(f"""
+                            SELECT word
+                            FROM normand
+                            WHERE EXISTS (
+                                SELECT word
+                                FROM normand
+                                WHERE word = '{wordNormand}'
+                            )
+                        """)
+                        result = c.fetchmany(1)
+
+                        if (len(result) > 0): 
+                            print(f"[DATABASE][INTABLE] {currentTime} le mot {wordNormand} est présent dans la table 'normand'.")
+                            return True
+                        else:
+                            print(f"[DATABASE][INTABLE] {currentTime} le mot {wordNormand} n'est pas présent dans la table 'normand'.")
+                    except mysql.connector.Error as err:
+                        error1 = str(err)
+                        db.rollback()
+                        db.close()
+                        print(f"[DATABSE][INTABLE] {currentTime} {error1}")
+                        return True
+                    
+                    return False
+        except mysql.connector.Error as err:
+            error1 = str(err)
+            print(f"[DATABASE][INTABLE] {currentTime} Impossible de se connecter à la base de données")
+            print(f"[DATABASE][INTABLE] {currentTime} {error1}")
+            db.close()
+            
+    def addWord(self,wordFrench:str,wordNormand:str): #Ajoutes des mots dans la base de donnée
+        currentTime = time.strftime("%x-%X")
+
+        if (self.inTable(wordFrench,wordNormand) == False):
+            print(f"[DATABASE][ADDWORD] {currentTime} Ajout du mot français {wordFrench} et du mot normand {wordNormand} dans la base de données.")
+        else:
+            print(f"[DATABASE][ADDWORD] {currentTime} Les mots {wordFrench} et {wordNormand} sont déjà dans la base de donnée.")
+            return 
+
+        try:
+            with mysql.connector.connect(**self.connexion) as db:
+                with db.cursor() as c:
+                    
+                    #Ajoutes le mot français dans la table 'french'
+                    print(f"[DATABASE][ADDWORD] {currentTime} INSERT INTO french (word) VALUES ('{wordFrench}')")
+                    try:
+                        c.execute(f"INSERT INTO french (word) VALUES ('{wordFrench}')")
+                        db.commit()
+                        print(f"[DATABASE][ADDWORD] {currentTime} COMMIT effectué!")
+                    except mysql.connector.Error as err:
+                        db.rollback()
+                        db.close()
+                        error1 = str(err)
+                        print(f"[DATABSE][ADDWORD] {currentTime} {error1}")
+                    
+                    #Ajoutes le mot normand dans la table 'normand'
+                    print(f"[DATABASE][ADDWORD] {currentTime} INSERT INTO normand (expression) VALUES ('{wordNormand}')")
+                    try:
+                        c.execute(f"INSERT INTO normand (word) VALUES ('{wordNormand}')")
+                        db.commit()
+                        print(f"[DATABASE][ADDWORD] {currentTime} COMMIT effectué!")
+                    except mysql.connector.Error as err:
+                        db.rollback()
+                        db.close()
+                        error1 = str(err)
+                        print(f"[DATABSE][ADDWORD] {currentTime} {error1}")
+
+                    #Modifies la colonne normandID du mot français qui est lié
+                    print(f"[DATABASE][ADDWORD] {currentTime} UPDATE french SET normandID = (SELECT ID FROM normand WHERE word = '{wordNormand}')")
+                    try:
+                        c.execute(f"UPDATE french SET normandID = (SELECT ID FROM normand WHERE word = '{wordNormand}')")
+                        db.commit()
+                        print(f"[DATABASE][ADDWORD] {currentTime} COMMIT effectué!")
+                    except mysql.connector.Error as err:
+                        db.rollback()
+                        db.close()
+                        error1 = str(err)
+                        print(f"[DATABSE][ADDWORD] {currentTime} {error1}")
+                    
+                    #Modifies la colonne frenchID du mot normand qui est lié
+                    print(f"[DATABASE][ADDWORD] {currentTime} UPDATE normand SET frenchID = (SELECT ID FROM french WHERE word = '{wordNormand}')")
+                    try:
+                        c.execute(f"UPDATE normand SET frenchID = (SELECT ID FROM french WHERE word = '{wordFrench}')")
+                        db.commit()
+                        print(f"[DATABASE][ADDWORD] {currentTime} COMMIT effectué!")
+                    except mysql.connector.Error as err:
+                        db.rollback()
+                        db.close()
+                        error1 = str(err)
+                        print(f"[DATABSE][ADDWORD] {currentTime} {error1}")
+            print(f"[DATABASE][ADDWORD] {currentTime} Les mots {wordFrench} et {wordNormand} ont été rajoutés dans les tables.")
+            db.close()
+
+        except mysql.connector.Error as err:
+            error1 = str(err)
+            print(f"[DATABASE][INTABLE] {currentTime} Impossible de se connecter à la base de données")
+            print(f"[DATABASE][INTABLE] {currentTime} {error1}")
+            db.close()
+
+    def removeWord(self,ID):
+        currentTime = time.strftime("%x-%X")
+        try:
+            with mysql.connector.connect(**self.connexion) as db:
+                with db.cursor() as c:
+                    
+                    c.execute(f"SET FOREIGN_KEY_CHECKS=0;")
+                    db.commit()
+                    
+                    print(f"[DATABASE][REMOVEWORD] DELETE FROM french WHERE ID = {ID}")
+                    try:
+                        c.execute(f"DELETE FROM french WHERE ID = {ID}")
+                        db.commit()
+                        print(f"[DATABASE][REMOVEWORD] {currentTime} COMMIT executé.")
+                    except:
+                        db.rollback()
+                        print(f"[DATABASE][REMOVEWORD] {currentTime} COMMIT non executé.")
+
+                    print(f"[DATABASE][REMOVEWORD] DELETE FROM normand WHERE ID = {ID}")
+                    try:
+                        c.execute(f"DELETE FROM normand WHERE ID = {ID}")
+                        db.commit()
+                        print(f"[DATABASE][REMOVEWORD] {currentTime} COMMIT executé.")
+                    except:
+                        db.rollback()
+
+                    c.execute(f"SET FOREIGN_KEY_CHECKS=1;")
+                    db.commit()
+
+        except mysql.connector.Error as err:
+            error1 = str(err)
+            print(f"[DATABASE][INTABLE] {currentTime} Impossible de se connecter à la base de données")
+            print(f"[DATABASE][INTABLE] {currentTime} {error1}")
+            db.close()
+
+    def modifyWord(self,ID,frenchWord,normandWord):
+        pass
+                
 access = None
 
 try:
@@ -78,6 +265,7 @@ except:
     access = None
 
 DATABASE = database(access['host'],access['user'],access['password'],access['database'])
-                
+#DATABASE.addWord("bonjour","bonjou")
+DATABASE.addWord("bonjour","bonjou")
                 
 
